2c41fe21d8a40f433da508b440a4c1f9b8b64ee5
Rasyid-deve <renaldirasyed@gmail.com>
2022-12-01 06:57:18 +0700

    Update Controller, Model, Migrations dan halaman adminTanya

--- laravel-api/resources/views/admin/adminTanya.blade.php
+++ laravel-api/resources/views/admin/adminTanya.blade.php
@@ -111,7 +111,9 @@
                                 <p class="font-semibold pt-5 text-black capitalize">Sampaikan Informasi</p>
 
                                 <!--start form Pertanyaan -->
-                                <form>
+                                <form method="POST" action="{{ route('category.store') }}" enctype="multipart/form-data">
+                                    @csrf
+                                    
                                     <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
                                         <div class="flex items-center justify-between px-3 py-2 border-b dark:border-gray-600">
                                             <div class="flex flex-wrap items-center divide-gray-200 sm:divide-x dark:divide-gray-600">
